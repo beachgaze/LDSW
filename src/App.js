ede877e5e240ec8f84e59312891ed3c774b4db6f
jorge velazquez <jorgevelazquez@jorges-MacBook-Air.local>
2021-05-05 21:12:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
+import logo from './logos.png';
 import './App.css';
+import pdf from './catalog.pdf';
+
 
 function App() {
   return (
     <div className="container">
+   
+
+<img alt='' class="logo" src={logo} />
+
    <h1 className="title">
-      Hello world <span aria-label="emoji" role="img">🔥</span>
+      KUGART SERVICES <span aria-label="emoji" role="img">🔥</span>
    </h1>
-   <input type="text" className="new-task"/>
+
+<img alt='' class="img" src= 'http://ugartrade.com/wp-content/uploads/2020/11/truck-3559573_1280.jpg'/>
+<br/>
+<hr/><hr/>
+
+<br/><br/>
+<h2 className="title">
+     CATALOG
+   </h2>
+
+<iframe alt='' src={pdf} width="100%" height="700px">
+    </iframe>
+
+<br/><br/>
+<hr/>
+
+<br/><br/>
+
 </div>
   );
 }
